00c96d131d681b8861eec9c8fdcbb04fc26e2a81
Vitaly D <vitcode.mail@gmail.com>
2025-02-09 10:52:09 +0300

    Celery beat sender simple example updated

--- fastapi_notifications/src/tasks/sender.py
+++ fastapi_notifications/src/tasks/sender.py
@@ -22,45 +22,49 @@ def process_message(message: str):
 def sender_task(self, name: str) -> None:
     try:
         log.info(f"\n{'-'*30}\n{name} launched.\n")
-        credentials = PlainCredentials("user", "password")
-        connection = BlockingConnection(
+        credentials = PlainCredentials(username="user", password="password")
+        with BlockingConnection(
             ConnectionParameters(host="localhost", credentials=credentials)
-        )
-        channel = connection.channel()
+        ) as connection:
+            with connection.channel() as channel:
+                log.debug(f"Connected successfully to RabbitMQ.")
 
-        exchange_name = EXCHANGE_NAME
-        channel.exchange_declare(
-            exchange=exchange_name, exchange_type=ExchangeType.topic
-        )
-        # queue = channel.queue_declare(queue="", exclusive=True)
-        # queue_name = queue.method.queue
-        ###
-        queue_name = "notifications"
-        log.info(f"\nqueue_name: {queue_name}")
+                exchange_name = EXCHANGE_NAME
+                channel.exchange_declare(
+                    exchange=exchange_name, exchange_type=ExchangeType.topic
+                )
+                # queue = channel.queue_declare(queue="", exclusive=True)
+                # queue_name = queue.method.queue
+                ###
+                queue_name = "notifications"
+                log.info(f"\nqueue_name: {queue_name}")
 
-        binding_keys = ["#"]
-        for binding_key in binding_keys:
-            channel.queue_bind(
-                exchange=exchange_name,
-                queue=queue_name,
-                routing_key=binding_key,
-            )
+                binding_keys = ["#"]
+                for binding_key in binding_keys:
+                    channel.queue_bind(
+                        exchange=exchange_name,
+                        queue=queue_name,
+                        routing_key=binding_key,
+                    )
 
-        method_frame, header_frame, body = channel.basic_get(queue_name)
-        log.info(
-            f"\n(method_frame, header_frame, body): \n{(method_frame, header_frame, body)}\n\n"
-        )
-        if method_frame is not None:
-            message_body = body.decode("utf-8")
-            log.info(f"Got a message: {message_body}")
+                method_frame, header_frame, body = channel.basic_get(
+                    queue_name
+                )
+                log.info(
+                    f"\n(method_frame, header_frame, body): \n{(method_frame, header_frame, body)}\n\n"
+                )
+                if method_frame is not None:
+                    message_body = body.decode("utf-8")
+                    log.info(f"Got a message: '{message_body}'")
 
-            # Celery task 'process_message'
-            process_message.delay(message_body)
+                    # Celery task 'process_message'
+                    process_message.delay(message_body)
 
-            # Confirmation of receipt of the message
-            channel.basic_ack(method_frame.delivery_tag)
+                    # Confirmation of receipt of the message
+                    channel.basic_ack(method_frame.delivery_tag)
+                else:
+                    log.info("\nNo messages available.\n")
 
-        connection.close()
         log.info(f"Connection closed.\n\n{'-'*30}\n")
     except AMQPConnectionError as err:
         log.info(f"An error connecting to RabbitMQ: {err}")
