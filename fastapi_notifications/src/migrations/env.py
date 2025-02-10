036456deeeacb80494566a5e10d66b82c0a47c9f
Vitaly D <vitcode.mail@gmail.com>
2025-02-10 22:03:52 +0300

    command: alembic revision --autogenerate -m "notifications_initial"

--- fastapi_notifications/src/migrations/env.py
+++ fastapi_notifications/src/migrations/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from db.postgres import Base, dsn
+from models.notification import Notification  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +15,13 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option("sqlalchemy.url", dsn + "?async_fallback=True")
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
