25e90ed10d76131b445a3a9c43f7ef17b686170f
Vitaly D <vitcode.mail@gmail.com>
2025-02-15 18:48:22 +0300

    Messages generator updated

--- fastapi_notifications/src/scripts/send_messages.py
+++ fastapi_notifications/src/scripts/send_messages.py
@@ -1,4 +1,5 @@
 import asyncio
+from random import choice, randint
 from typing import Any
 
 import httpx
@@ -6,9 +7,21 @@ import httpx
 
 def get_message_data(message: str) -> dict[str, Any]:
     message_data = {
-        "user_id": "00000000-0000-0000-0000-000000000001",
+        "user_id": choice(
+            [
+                "00000000-0000-0000-0000-000000000001",
+                "00000000-0000-0000-0000-000000000002",
+                "00000000-0000-0000-0000-000000000003",
+            ]
+        ),
         "template_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "subject": "Title",
+        "subject": choice(
+            [
+                "New films",
+                "Check bookmarks",
+                "New likes",
+            ]
+        ),
         "message": message,
         "notification_type": "email",
     }
@@ -17,6 +30,8 @@ def get_message_data(message: str) -> dict[str, Any]:
 
 
 def generate_notifications(quantity: int = 1000) -> list[dict[str, Any]]:
+    """Generates notifications."""
+
     notifications = [
         get_message_data(f"Message {i+1}") for i in range(quantity)
     ]
@@ -24,17 +39,26 @@ def generate_notifications(quantity: int = 1000) -> list[dict[str, Any]]:
 
 
 async def send_notification_task(notifications: list[dict[str, Any]]) -> None:
+    """Sends notifications."""
+
     async with httpx.AsyncClient() as client:
         url = "http://localhost:8006/api/v1/notifications/"
         for notification in notifications:
             response = await client.post(url=url, json=notification)
 
 
-async def main() -> None:
-    notifications_tasks = generate_notifications(1_000)
-    task = asyncio.create_task(send_notification_task(notifications_tasks))
-    await task
+async def main(
+    cicles_quantity: int = -1, _min: int = 10, _max: int = 100
+) -> None:
+    """The main function for generating notifications."""
+    counter = 0
+    while counter != cicles_quantity:
+        notifications_tasks = generate_notifications(randint(_min, _max))
+        task = asyncio.create_task(send_notification_task(notifications_tasks))
+        await task
+        await asyncio.sleep(randint(5, 20))
+        counter += 1
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main(cicles_quantity=1))
